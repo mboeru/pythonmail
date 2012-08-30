d8264469c23b0c883437be8015fae6c73de44ed6
mboeru <mboeru@gmail.com>
2012-08-30 12:16:54 +0300

    first commit

---
+++ send_mail.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+import os
+import sys
+import configparser
+
+config = configparser.ConfigParser()
+config.read_file(open('sendmail.ini'))
+
+# to subject text file
+
+sendto = sys.argv[1]
+subject = sys.argv[2]
+innertext = sys.argv[3]
+attfile = sys.argv[4]
+
+
+
+gmail_user = config['Gmail Account Info']['User']
+gmail_pwd = config['Gmail Account Info']['Password']
+
+def mail(to, subject, text, attach):
+   msg = MIMEMultipart()
+
+   msg['From'] = gmail_user
+   msg['To'] = to
+   msg['Subject'] = subject
+
+   msg.attach(MIMEText(text))
+
+   part = MIMEBase('application', 'octet-stream')
+   part.set_payload(open(attach, 'rb').read())
+   Encoders.encode_base64(part)
+   part.add_header('Content-Disposition',
+           'attachment; filename="%s"' % os.path.basename(attach))
+   msg.attach(part)
+
+   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+   mailServer.ehlo()
+   mailServer.starttls()
+   mailServer.ehlo()
+   mailServer.login(gmail_user, gmail_pwd)
+   mailServer.sendmail(gmail_user, to, msg.as_string())
+   # Should be mailServer.quit(), but that crashes...
+   mailServer.close()
+
+mail(sendto,
+   subject,
+   innertext,
+   attfile)
